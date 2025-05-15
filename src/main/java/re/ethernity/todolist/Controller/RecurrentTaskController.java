1d3be6ae259fbbc60d3f7fcdc8dbceb085c00b9e
3thernitydev <3thernitydev@gmail.com>
2025-05-15 16:18:03 +0400

    [ADD] RecurrentTaskController

---
+++ src/main/java/re/ethernity/todolist/Controller/RecurrentTaskController.java
@@ -0,0 +1,71 @@
+package re.ethernity.todolist.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import re.ethernity.todolist.Entity.RecurrentTask;
+import re.ethernity.todolist.repository.RecurrentTaskRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/recurrentTask/")
+public class RecurrentTaskController {
+
+    @Autowired
+    private RecurrentTaskRepository recurrentTaskRepository;
+
+    @GetMapping("/")
+    public List<RecurrentTask> getAllTasks() {
+        return (List<RecurrentTask>) recurrentTaskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTaskById(@PathVariable long id) {
+        Optional<RecurrentTask> task = recurrentTaskRepository.findById(id);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cette tâche récurrente n'a pas été trouvée. ID -> " + id);
+        }
+    }
+
+    @PostMapping("/")
+    public RecurrentTask save(@RequestBody RecurrentTask recurrentTask) {
+        return recurrentTaskRepository.save(recurrentTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTask(@PathVariable long id, @RequestBody RecurrentTask updatedTask) {
+        Optional<RecurrentTask> taskOptional = recurrentTaskRepository.findById(id);
+        if (taskOptional.isPresent()) {
+            RecurrentTask task = taskOptional.get();
+            task.setName(updatedTask.getName());
+            task.setDescription(updatedTask.getDescription());
+            task.setPriority(updatedTask.getPriority());
+            task.setFrequency(updatedTask.getFrequency());
+            task.setCategories(updatedTask.getCategories());
+
+            recurrentTaskRepository.save(task);
+            return ResponseEntity.ok(task);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Tâche récurrente non trouvée. ID -> " + id);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable long id) {
+        Optional<RecurrentTask> taskOptional = recurrentTaskRepository.findById(id);
+        if (taskOptional.isPresent()) {
+            recurrentTaskRepository.delete(taskOptional.get());
+            return ResponseEntity.ok("Tâche récurrente supprimée avec succès. ID -> " + id);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Tâche récurrente non trouvée. ID -> " + id);
+        }
+    }
+}
