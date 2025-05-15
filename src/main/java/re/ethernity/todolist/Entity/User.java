bee9f6910e2d811fdc47e9b39eecd464ea47939c
3thernitydev <3thernitydev@gmail.com>
2025-05-15 11:10:41 +0400

    [ADD] UserEntity

---
+++ src/main/java/re/ethernity/todolist/Entity/User.java
@@ -0,0 +1,60 @@
+package re.ethernity.todolist.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String Name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    private String Password;
+    private String Email;
+
+    public User() {
+
+    }
+
+    public User(String name, String password, String email) {
+        this.Name = name;
+        this.Password = password;
+        this.Email = email;
+    }
+
+}
